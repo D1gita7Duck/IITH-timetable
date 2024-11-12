a4c63510952543df988e36513517921d46dd2d5a
D1gita7Duck <147811924+D1gita7Duck@users.noreply.github.com>
2024-11-12 10:44:40 +0530

    initial commit

---
+++ commands.py
@@ -0,0 +1,32 @@
+import customtkinter as ctk
+import os
+
+def open_calendar_file():
+    pass
+
+def open_source_code():
+    pass
+
+def modify_a_slot(A_slot_btns : list[ctk.CTkButton]):
+    pass
+
+def modify_b_slot(B_slot_btns : list[ctk.CTkButton]):
+    pass
+
+def modify_c_slot(C_slot_btns : list[ctk.CTkButton]):
+    pass
+
+def modify_d_slot(D_slot_btns : list[ctk.CTkButton]):
+    pass
+
+def modify_e_slot(E_slot_btns : list[ctk.CTkButton]):
+    pass
+
+def modify_f_slot(F_slot_btns : list[ctk.CTkButton]):
+    pass
+
+def modify_g_slot(G_slot_btns : list[ctk.CTkButton]):
+    pass
+
+def modify_fn_an(btn : ctk.CTkButton):
+    pass
